dff72dd936a72f8ab597a3f2de4db7de40b1fc4e
Daniel Toro Nieto <danitn1997@gmail.com>
2018-03-07 12:06:10 +0100

    Genero crud con gii

---
+++ views/noticias/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Noticias */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="noticias-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'titulo')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'texto')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'img')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'creador_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
