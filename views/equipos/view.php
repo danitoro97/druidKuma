b61c80942922d49d1a434403c8e78dc2d917c0ad
Daniel Toro Nieto <danitn1997@gmail.com>
2018-05-30 15:20:46 +0200

    Docs y leve cambios

--- views/equipos/view.php
+++ views/equipos/view.php
@@ -34,6 +34,42 @@ $css = <<<EOT
 EOT;
 
 $this->registerCss($css);
+
+$this->registerJsFile('plugin/fullcalendar/lib/moment.min.js',['depends' => [\yii\web\JqueryAsset::className()]]);
+$this->registerJsFile('plugin/fullcalendar/fullcalendar.min.js',['depends' => [\yii\web\JqueryAsset::className()]]);
+$this->registerJsFile('plugin/fullcalendar/locale/es.js',['depends' => [\yii\web\JqueryAsset::className()]]);
+$this->registerCssFile('plugin/fullcalendar/fullcalendar.min.css');
+$ruta = Url::to(['partidos/partidos'], getenv('esquema'));
+$js=<<<EOT
+
+    $('#calendar').fullCalendar({
+        locale: 'es',
+        themeSystem: 'bootstrap3',
+        eventSources:[
+            {
+                url:'$ruta',
+                data: {
+
+                    liga:'$model->liga_id',
+                    id: '$model->id',
+                }
+            }
+
+        ],
+        defaultView: 'month',
+        eventClick: function(event) {
+
+            var y=parseInt((window.screen.height/2)-375);
+            var x=parseInt((window.screen.width/2)-500);
+           if (event.url) {
+             window.open(event.url,'Partidos',"width=1000,height=750, top="+y+",left="+x);
+             return false;
+           }
+        },
+    });
+EOT;
+
+$this->registerJs($js);
 ?>
 <div class="equipos-view">
     <div class="container">
@@ -78,5 +114,14 @@ $this->registerCss($css);
 
         </div>
 
+
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Proximos partidos</h2>
+                <div id="calendar">
+
+                </div>
+            </div>
+        </div>
     </div>
 </div>
